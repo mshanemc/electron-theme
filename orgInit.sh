612ef3d234eb6016d1d454afb787da4e22c13fae
mshanemc <shane.mclaughlin@salesforce.com>
2019-02-27 14:02:40 -0600

    json on the reauth

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
-sfdx shane:org:reauth -r
+sfdx shane:org:reauth -r --json
 sfdx shane:theme:activate -n Electron
 sfdx force:org:open -p /lightning/setup/ThemingAndBranding/home
